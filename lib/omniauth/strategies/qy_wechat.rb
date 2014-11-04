86e3ca496b2fa65c1d77d503b0f8608cdf1ba135
Special Leung <specialcyci@gmail.com>
2014-11-04 12:31:54 +0800

    update version.

--- lib/omniauth/strategies/qy_wechat.rb
+++ lib/omniauth/strategies/qy_wechat.rb
@@ -29,17 +29,19 @@ module OmniAuth
       def request_phase
         params = client.auth_code.authorize_params.merge(redirect_uri: callback_url).merge(authorize_params)
         params["appid"] = params.delete("client_id")
-        session['omniauth.corpid']  = params['appid']
-        session['omniauth.agentid'] = options.agentid
+        session['omniauth.corpid']     = params['appid']
+        session['omniauth.corpsecret'] = options.client_secret
+        session['omniauth.agentid']    = options.agentid
         redirect client.authorize_url(params)
       end
 
       def auth_hash
         hash = AuthHash.new(
-          provider: name,
-          code:     request.params['code'],
-          corpid:   request.session.delete('omniauth.corpid'),
-          agentid:  request.session.delete('omniauth.agentid')
+          provider:   name,
+          code:       request.params['code'],
+          corpid:     request.session.delete('omniauth.corpid'),
+          corpsecret: request.session.delete('omniauth.corpsecret'),
+          agentid:    request.session.delete('omniauth.agentid')
         )
         hash
       end
